19a33bc9698ed6b24008604f48eb78f4c2982ba8
Jan Bodnar <jan.bodnar@gmail.com>
2018-10-29 11:01:47 +0100

    Create Commons.java

---
+++ src/com/zetcode/Commons.java
@@ -0,0 +1,15 @@
+package com.zetcode;
+
+public interface Commons {
+    
+    public static final int WIDTH = 300;
+    public static final int HEIGTH = 400;
+    public static final int BOTTOM_EDGE = 390;
+    public static final int N_OF_BRICKS = 30;
+    public static final int INIT_PADDLE_X = 200;
+    public static final int INIT_PADDLE_Y = 360;
+    public static final int INIT_BALL_X = 230;
+    public static final int INIT_BALL_Y = 355;    
+    public static final int DELAY = 1000;
+    public static final int PERIOD = 10;
+}
