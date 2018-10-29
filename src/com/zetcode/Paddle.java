a0be67d7e5e92507b6bdd013aa53f7f0993f0ce2
Jan Bodnar <jan.bodnar@gmail.com>
2018-10-29 11:02:06 +0100

    Create Paddle.java

---
+++ src/com/zetcode/Paddle.java
@@ -0,0 +1,73 @@
+package com.zetcode;
+
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+
+public class Paddle extends Sprite implements Commons {
+
+    private int dx;
+
+    public Paddle() {
+        
+        initPaddle();        
+    }
+    
+    private void initPaddle() {
+
+        loadImage();
+        getImageDimensions();
+
+        resetState();
+    }
+    
+    private void loadImage() {
+        
+        ImageIcon ii = new ImageIcon("src/resources/paddle.png");
+        image = ii.getImage();        
+    }    
+
+    public void move() {
+
+        x += dx;
+
+        if (x <= 0) {
+            x = 0;
+        }
+
+        if (x >= WIDTH - imageWidth) {
+            x = WIDTH - imageWidth;
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -1;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 1;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+    }
+
+    private void resetState() {
+
+        x = INIT_PADDLE_X;
+        y = INIT_PADDLE_Y;
+    }
+}
